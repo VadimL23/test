bb20df642b5347d550ecd4da096e85c7a15a981e
Vadim <Vadim_lachkov@mail.ru>
2021-09-21 17:12:33 +0700

    second test

--- src/first.test.js
+++ src/first.test.js
@@ -1,36 +1,74 @@
 //first.test.js
 const axios = require('axios');
 const qs = require('qs');
+const faker = require('faker/locale/ru');
 
-test('Test express. This create the user and tests his ballance.', async () => {
-await axios.post(
+const phone = faker.phone.phoneNumber('89' + '#########').toString();
+
+test('Test express. This create the user.', async () => {
+  await axios
+    .post(
       'https://api.findinamika.com/api/v1/Users',
       qs.stringify({
-        login: '89041340559',
+        login: `${phone}`,
         role: 'passenger',
         start_balance: 100,
       })
-    ).then(data=>{
-    expect(data.status).toBe(200);
-})
-
-});
-
-
-test('Test express. This check the user ballance.', async () => {
-await axios.get(
-      'https://api.findinamika.com/api/v1/wallet',
-    {params:{
-        login: '89041340559',
-       }}
     )
     .then((data) => {
-    data.data.data.balance.forEach((el)=>{
-        if (el.asset_issuer_type == "currency"){
-            console.log(el.balance)
-          expect(el.balance).toBe(100);  
-        }
+      expect(data.data).toEqual({
+        success: true,
+        data: {
+          login: `${phone}`,
+          role: 'passenger',
+        },
+      });
+    });
+}, 20000);
+
+test('Test express. This try find user after create.', async () => {
+  await axios
+    .get(`https://api.findinamika.com/api/v1/users/${phone}`)
+    .then((data) => {
+      expect(data.data.data).toEqual({ login: `${phone}`, role: 'passenger' });
+    });
+});
+
+test('Test express. This check the user ballance.', async () => {
+  await axios
+    .get('https://api.findinamika.com/api/v1/wallet', {
+      params: {
+        login: `${phone}`,
+      },
     })
-    
+    .then((data) => {
+      data.data.data.balance.forEach((el) => {
+        if (el.asset_issuer_type == 'currency') {
+          expect(el.balance).toBe(100);
+        }
+      });
+    });
+});
+
+test('Test express. This delete the user.', async () => {
+  await axios
+    .delete('https://api.findinamika.com/api/v1/users', {
+      params: {
+        login: `${phone}`,
+      },
+    })
+    .then((data) => {
+      expect(data.data.success).toBe(true);
+    });
+});
+
+test('Test express. This try find user after delete.', async () => {
+  await axios
+    .get(`https://api.findinamika.com/api/v1/users/${phone}`)
+    .then((data) => {
+      expect(data.data.data).not.toEqual({
+        login: `${phone}`,
+        role: 'passenger',
+      });
     });
 });
